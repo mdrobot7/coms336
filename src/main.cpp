8eb80527bc694fa6ddbbb796623a074b806c4c15
mdrobot7 <55768773+mdrobot7@users.noreply.github.com>
2025-10-08 18:45:24 -0500

    main: Command line args

--- src/main.cpp
+++ src/main.cpp
@@ -1,33 +1,93 @@
 #include <iostream>
+#include <unistd.h>
+#include <string>
+#include <ctime>
 #include "render.hpp"
 #include "scene.hpp"
 
-/*
-Data related to how things are placed in the scene is
-passed through the scene file. These args are related
-to how the renderer generates the scene.
-
-Render args:
-- resolution
-- anti-aliasing level
-- scene file
-
-Meta:
-- job count
-*/
+#define HELP                                                                      \
+    "COMS 336 Ray Tracing Renderer\n"                                             \
+    "usage: render.exe [options]\n\n"                                             \
+    "options:\n"                                                                  \
+    "-h                 Show this help message and exit\n"                        \
+    "-s [SCENE_JSON]    Scene file for the renderer.\n"                           \
+    "                       Default: scenes/sample.json\n"                        \
+    "-r [RESOLUTION]    Resolution for the renderer, in the form\n"               \
+    "                       [width]x[height]. Default: 1024x768\n"                \
+    "-a [AA_LEVEL]      Anti-aliasing level (number of rays per pixel).\n"        \
+    "                       Default: 1\n"                                         \
+    "-d [DEPTH]         Max ray depth (number of bounces). Default: 50\n"         \
+    "-j [JOBS]          Job count. Default: 1\n"                                  \
+    "-o [OUTPUT]        Output file path. Outputs [OUTPUT].ppm (packed binary)\n" \
+    "                       and [OUTPUT].txt.ppm (text). Default: render\n"
 
 int main(int argc, char *argv[])
 {
-    Scene s;
-    std::cout << "Building scene..." << std::endl;
-    s.load("scenes/test.json");
+    int opt;
+    std::string scenePath = "scenes/sample.json";
+    int width = 1024, height = 768;
+    int antiAliasingLevel = 1;
+    int depth = 50;
+    int jobs = 1;
+    std::string outputPath = "render";
+    while ((opt = getopt(argc, argv, "hs:r:a:d:j:o:")) != -1)
+    {
+        switch (opt)
+        {
+        case 'h':
+            std::cout << HELP << std::endl;
+            return 0;
+        case 's':
+            scenePath = std::string(optarg);
+            break;
+        case 'r':
+        {
+            std::string optargStr(optarg);
+            std::string widthStr = optargStr.substr(0, optargStr.find('x'));
+            std::string heightStr = optargStr.substr(optargStr.find('x') + 1);
+            width = (int)std::stoul(widthStr);
+            height = (int)std::stoul(heightStr);
+            break;
+        }
+        case 'a':
+            antiAliasingLevel = (int)std::stoul(optarg);
+            break;
+        case 'd':
+            depth = (int)std::stoul(optarg);
+            break;
+        case 'j':
+            jobs = (int)std::stoul(optarg);
+            break;
+        case 'o':
+            outputPath = std::string(optarg);
+            break;
+        default:
+            return 1;
+        }
+    }
 
-    Render render(s, 1024, 768, 1, 10);
-    std::cout << "Launching renderer..." << std::endl;
-    render.run();
-    std::cout << "Saving output..." << std::endl;
-    render.save("test");
+    try
+    {
+        std::time_t startTime = std::time(NULL);
 
-    std::cout << "Done." << std::endl;
+        Scene s;
+        std::cout << "Building scene..." << std::endl;
+        s.load(scenePath);
+
+        Render render(s, width, height, antiAliasingLevel, jobs, depth);
+        std::cout << "Launching renderer..." << std::endl;
+        render.run();
+        std::cout << "Saving output..." << std::endl;
+        render.save(outputPath);
+
+        char timeElapsed[9];
+        time_t interval = std::time(NULL) - startTime;
+        strftime(timeElapsed, 9, "%T", gmtime(&interval));
+        std::cout << "Done. Render completed in " << timeElapsed << "." << std::endl;
+    }
+    catch (const std::exception &e)
+    {
+        std::cout << "Exception " << e.what() << std::endl;
+    }
     return 0;
 }
